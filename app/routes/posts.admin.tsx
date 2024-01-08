50a5f1790ebc283a50a4cbce784b7d32985a1e51
issei-yoshi <iyoshi.engineer@gmail.com>
2024-01-08 13:11:39 +0900

    defined nested routes for admin

--- app/routes/posts.admin.tsx
+++ app/routes/posts.admin.tsx
@@ -1,5 +1,9 @@
 import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import {
+  Link,
+  Outlet,
+  useLoaderData,
+} from "@remix-run/react";
 
 import { getPosts } from "~/models/post.server";
 
@@ -30,7 +34,7 @@ export default function PostAdmin() {
           </ul>
         </nav>
         <main className="col-span-4 md:col-span-3">
-          ...
+          <Outlet />
         </main>
       </div>
     </div>
